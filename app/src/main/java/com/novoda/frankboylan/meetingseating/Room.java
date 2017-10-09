3f19773fc2d5a111b183f3528d499217fc970773
frankboylan <frankieboylan@hotmail.co.uk>
2017-10-09 13:34:02 +0100

    *Reformat cont* - Cleaned Room & Seat objects

--- app/src/main/java/com/novoda/frankboylan/meetingseating/Room.java
+++ app/src/main/java/com/novoda/frankboylan/meetingseating/Room.java
@@ -1,33 +1,33 @@
 package com.novoda.frankboylan.meetingseating;
 
-public class Room {
+class Room {
     private String roomName;
     private int roomId;
     private String roomLocation;
     private String roomUnitName;
 
-    public void setRoomName(String roomName) {
+    void setRoomName(String roomName) {
         this.roomName = roomName;
     }
-    public String getRoomName() {
+    String getRoomName() {
         return roomName;
     }
-    public void setRoomId(int roomId) {
+    void setRoomId(int roomId) {
         this.roomId = roomId;
     }
-    public int getRoomId() {
+    int getRoomId() {
         return roomId;
     }
-    public void setRoomLocation(String roomLocation) {
+    void setRoomLocation(String roomLocation) {
         this.roomLocation = roomLocation;
     }
-    public String getRoomLocation() {
+    String getRoomLocation() {
         return roomLocation;
     }
-    public void setRoomUnitName(String roomUnitName) {
+    void setRoomUnitName(String roomUnitName) {
         this.roomUnitName = roomUnitName;
     }
-    public String getRoomUnitName() {
+    String getRoomUnitName() {
         return roomUnitName;
     }
     @Override
--- app/src/main/java/com/novoda/frankboylan/meetingseating/Seat.java
+++ app/src/main/java/com/novoda/frankboylan/meetingseating/Seat.java
@@ -1,31 +1,31 @@
 package com.novoda.frankboylan.meetingseating;
 
-public class Seat {
+class Seat {
     private int seatId, value, roomId;
     private String unitType;
 
-    public void setSeatId(int seatId) {
+    void setSeatId(int seatId) {
         this.seatId = seatId;
     }
-    public int getSeatId() {
+    int getSeatId() {
         return seatId;
     }
-    public void setValue(int value) {
+    void setValue(int value) {
         this.value = value;
     }
-    public int getValue() {
+    int getValue() {
         return value;
     }
-    public void setUnitType(String unitType) {
+    void setUnitType(String unitType) {
         this.unitType = unitType;
     }
-    public String getUnitType() {
+    String getUnitType() {
         return unitType;
     }
-    public void setRoomId(int roomId) {
+    void setRoomId(int roomId) {
         this.roomId = roomId;
     }
-    public int getRoomId() {
+    int getRoomId() {
         return roomId;
     }
     @Override
