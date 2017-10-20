b97936e774da3e5b2b81959a396d345eb1daeafe
frankboylan <frankieboylan@hotmail.co.uk>
2017-10-20 10:28:06 +0100

    implement Firebase login, with basic valdiation checks.

--- app/src/main/java/com/novoda/frankboylan/meetingseating/LoginActivity.java
+++ app/src/main/java/com/novoda/frankboylan/meetingseating/LoginActivity.java
@@ -2,23 +2,67 @@ package com.novoda.frankboylan.meetingseating;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
+    private FirebaseAuth auth;
+    TextView tvEmail, tvPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, StatisticsActivity.class));
+            finish();
+        }
+
+        tvEmail = findViewById(R.id.tv_login_email);
+        tvPassword = findViewById(R.id.tv_login_password);
+
     }
 
     /**
      * Button Handler method - Login
      */
     public void handlerLogin(View v) {
-        // Authenticate with Firebase -> Feedback to user
+        String email = tvEmail.getText().toString();
+        String pass = tvPassword.getText().toString();
+
+        if (email.isEmpty() || pass.isEmpty()) {
+            makeToast("Missing fields!");
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(LoginActivity.this, StatisticsActivity.class));
+                            finish();
+                        }
+                        makeToast("Username & Password Combo not recognised!");
+                    }
+                });
+
+    }
+
+    private void makeToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     /**
